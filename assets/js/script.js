fc0a59e6081fb339e5fb7cc4a62a71f27024e342
llattanzi <lauren71442@gmail.com>
2022-03-25 12:59:38 -0400

    display current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
+// constants
+var uvFavorableLimit = 4;
+var uvModerateLimit = 7;
+var forecastDays = 5;
+
 var getCoordinates = function(city) {
     // format Geocoding api url
     var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=b382fc1462adf9eba28383207f67b071";
@@ -41,12 +46,31 @@ var getCurrentWeather = function(lat, lon, city) {
 };
 
 var displayWeather = function(data, city) {
+    // get current date wrt city timezone
     var currentDate = moment().tz(data.timezone).format("M/DD/YYYY");
+    // build city name, current date, icon to apply to page
     var cityInfo = city + " (" + currentDate + ") ";
     $("#city-info").text(cityInfo);
     var iconCode = data.current.weather[0].icon;
     var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
     $("#current-icon").attr('src', iconUrl);
+
+    // add current weather info to page
+    $(".temperature").eq(0).text(data.current.temp);
+    $(".wind").eq(0).text(data.current.wind_speed);
+    $(".humidity").eq(0).text(data.current.humidity);
+    $(".uv").eq(0).text(data.current.uvi);
+
+    // overwrite all classes with class uv to style based on uv index severity
+    if (data.current.uvi < uvFavorableLimit) {
+        $(".uv").eq(0).attr("class", "uv uv-favorable");
+    }
+    else if (data.current.uvi < uvModerateLimit) {
+        $(".uv").eq(0).attr("class", "uv uv-moderate");
+    }
+    else {
+        $(".uv").eq(0).attr("class", "uv uv-severe");
+    }
 }
 
 $(".btn-search").click(function() {
